bf1486579838ebe027d66e715f10e037c3dcbd37
Saulm <Saulm@SAUL>
2023-03-15 21:37:13 -0600

    Agregar archivos de proyecto.

---
+++ Binarios/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    FileStream archivo = new FileStream("Archivo.bin", FileMode.Open);
+    BinaryReader lector = new BinaryReader(archivo);
+    Console.WriteLine(lector);
+    archivo.Close();
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine("El archivo no se encontró.");
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Ha ocurrido un error inesperado.");
+    Console.WriteLine(e.Message);
+}
